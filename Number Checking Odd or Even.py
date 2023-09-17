a1369d1df303b5ccecac29df3a25680c135d418a
Talha Iftikar <92679085+TalhaIftikharAli@users.noreply.github.com>
2023-09-17 16:42:29 +0500

    Add files via upload

---
+++ Number Checking Odd or Even.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# """
+# Created on Sun Sep 17 16:37:57 2023
+
+# @author: talha.i
+# """
+
+# Number Checking - Odd or Even
+
+number = int(input("Which number do you want to check?\n"))
+
+if number % 2 == 0:
+    print("This is an even number")
+else:
+    print("This is an odd number")
